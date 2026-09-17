d086a3eda8cf63c54c9b93d5a571f1bf4be56a17
vercidePL <avanebet@gmail.com>
2026-09-18 01:37:36 +0300

    Delete media_handlers.py

--- media_handlers.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Media/service-message handling boundary for the settings subsystem."""
-from settings_core import _extract_media, _on_media_message, _on_service_message
-
-extract_media = _extract_media
-on_media_message = _on_media_message
-on_service_message = _on_service_message
-
-__all__ = ["extract_media", "on_media_message", "on_service_message"]
